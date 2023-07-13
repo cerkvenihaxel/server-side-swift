2cf5f3e2835d91988e27b9faa62c36682a76944d
cerkvenihaxel <cerkvenihaxel@gmail.com>
2023-07-12 22:40:56 -0300

    feat 'First commit GET and DELETE done'

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,18 +7,16 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     app.databases.use(.postgres(configuration: SQLPostgresConfiguration(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+        password: Environment.get("DATABASE_PASSWORD") ?? "axel39300300",
+        database: Environment.get("DATABASE_NAME") ?? "exam",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
-    app.migrations.add(CreateTodo())
-
+    app.migrations.add(CreateExamTable())
     // register routes
     try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,44 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+   
+    //This function returns all data inside Exam table
+    app.get("exam"){req -> [Exam] in
+        return try await Exam.query(on: req.db).all()
     }
+    
+    //This function retrieve all the exams given a specified id
+       app.get("exam",":id"){req -> [Exam] in
+           guard let number = req.parameters.get("id", as: String.self) else {
+               throw Abort(.badRequest)
+           }
+           return try await Exam.query(on: req.db).filter(\.$exam_id == number).all()
+       }
+    
+    //This function create a new exam using a json file
+      app.post("exam"){req -> Exam in
+          let data = try req.content.decode(Exam.self)
+          let exam = Exam(id: data.id, name: data.name, course: data.course, exam_id: data.exam_id)
+          
+          try await exam.save(on: req.db)
+          
+          return exam
+      }
+    
+    
+    //This function delete a specific exam using an id
+       app.delete("exam",":id"){req -> String in
+           guard let number = req.parameters.get("id", as: String.self) else {
+               throw Abort(.badRequest)
+           }
+           
+           Exam.query(on: req.db).filter(\.$exam_id == number).delete()
+           return "Success"
+       }
+    
+    
+    
+    
+    
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-
-    try app.register(collection: TodoController())
 }
